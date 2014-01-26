eb34e7d175c86378f9ce5978b8b243c90254e743
Matthew Brown <matt@ederoyd.co.uk>
2014-01-26 18:57:59 +0000

    update

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Text.CSV(parseCSVFromFile, Field, CSV)
-import Data.List(map)
+import Data.List(map, elemIndex)
 import Control.Monad(mapM_)
 import qualified PB.Index.Header as Header
 import qualified PB.Index.Entry as Entry
-import Text.ProtocolBuffers.Basic (uFromString, Int64, Seq, Utf8)
+import Text.ProtocolBuffers.Basic (ByteString, uFromString, uToString, Int64, Seq, Utf8)
 import Text.ProtocolBuffers (getVal)
 import Data.Sequence(fromList)
 import Text.ProtocolBuffers.WireMessage (messageGet, messagePut)
+import Data.Binary (encode)
+import Data.Binary.Get (Get, getWord64be, getLazyByteString, runGet, bytesRead, skip)
+import Data.Foldable (toList)
+
 import qualified Data.ByteString.Lazy as ByteString (readFile, writeFile, length, appendFile)
 
 --import Codec.Compression.Zlib as Zlib (compress, decompress)
@@ -17,7 +21,7 @@ import Data.Int
 
 main :: IO ()
 main = do
-	csv <- parseCSVFromFile "/var/development/geodata-small.csv"
+	csv <- parseCSVFromFile "/var/development/geodata.csv"
 	case csv of
 		Right d -> decodeFile d
 		Left err -> putStrLn "File has no data"
@@ -25,16 +29,21 @@ main = do
 decodeFile :: CSV -> IO ()
 decodeFile csv = do
 	putStrLn $ "File has " ++ (show $ length csv) ++ " entries"
-	let rows = filter (isJust) $ map (buildEntry) csv
-	let bin = map (messagePut . fromJust) rows
-	let terms = map (getSearchTerm . fromJust) rows
-	let size = deltaEncode $ map (ByteString.length) bin
-	print $ buildIndexHeader terms size
-	writeToFile $ messagePut $ buildIndexHeader terms size
-	mapM_ (writeToFile) bin
+	let entries = filter (isJust) $ map (buildEntry) csv
+	let byteEntries = map (messagePut . fromJust) entries
+	let byteEntrySizes = deltaEncode $ map (ByteString.length) byteEntries
+	let terms = map (getSearchTerm . fromJust) entries
+	let header = messagePut $ buildIndexHeader terms byteEntrySizes
+	let headerSize = ByteString.length header :: Int64
+
+	writeToFile $ encode headerSize
+	writeToFile header
+	mapM_ (writeToFile) byteEntries
+
+	searchFile "LEEDS"
 
 	where
-		writeToFile = ByteString.appendFile "/tmp/test.pbf"
+		writeToFile = ByteString.appendFile "/tmp/test.pbf" 
 
 getSearchTerm :: Entry.Entry -> Utf8
 getSearchTerm e = getVal e Entry.term
@@ -52,3 +61,36 @@ deltaEncode a = head a : (zipWith (-) (tail a) a)
 
 deltaDecode :: [Int64] -> [Int64]
 deltaDecode a = scanl1 (+) a
+
+searchFile :: String -> IO ()
+searchFile s = do
+	handle <- ByteString.readFile "/tmp/test.pbf"
+	let header = runGet getHeader handle
+	let terms = map (uToString) $ toList $ getVal (fst header) Header.term 
+	let sizes = deltaDecode $ toList $ getVal (fst header) Header.size 
+	case (elemIndex s terms) of
+		Just i -> do
+			let offset = (snd header) + (foldl1 (+) (take i sizes))
+			let entrySize = sizes !! i
+			let entry = runGet (getEntry offset entrySize) handle
+			print entry
+		Nothing -> print "Does not exist"
+
+
+
+getHeader :: Get (Header.Header, Int64)
+getHeader = do
+    len <- getWord64be
+    headerBytes <- getLazyByteString (fromIntegral len)
+    let Right (header,_) = messageGet headerBytes ::  Either String (Header.Header, ByteString)
+    offset <- bytesRead
+    return (header, offset)
+
+getEntry :: Int64 -> Int64 -> Get Entry.Entry
+getEntry offset entrySize = do
+    _ <- skip $ fromIntegral offset
+    entryBytes <- getLazyByteString (fromIntegral entrySize)
+    let Right (entry,_) = messageGet entryBytes ::  Either String (Entry.Entry, ByteString)
+    return entry
+
+
