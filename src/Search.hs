fba4159ff11d5b9eca6391168161f52fd86816e3
Matthew Brown <matt@ederoyd.co.uk>
2014-01-28 12:27:09 +0000

    reduced number of conversions on the terms

--- src/Search.hs
+++ src/Search.hs
@@ -6,28 +6,31 @@ import Common
 import Data.List(map, elemIndex)
 import qualified PB.Index.Header as Header
 import qualified PB.Index.Entry as Entry
-import Text.ProtocolBuffers.Basic (ByteString, uToString, Int64)
+import Text.ProtocolBuffers.Basic (ByteString, uToString, uFromString, Int64)
 import Text.ProtocolBuffers (getVal)
 import Text.ProtocolBuffers.WireMessage (messageGet)
-import Data.Binary.Get (Get, getWord64be, getLazyByteString, runGet, bytesRead, skip)
+import Data.Binary.Get (Get, getWord64be, getByteString, getLazyByteString, runGet, bytesRead, skip)
 import Data.Foldable (toList)
 import qualified Data.ByteString.Lazy as ByteString (readFile, length)
+import qualified Data.ByteString as ByteString'
 --import Codec.Compression.Zlib as Zlib (compress, decompress)
+import Data.Sequence(elemIndexL,fromList)
 import Data.Int
 
 search :: String -> IO ()
 search s = do
 	handle <- ByteString.readFile "/tmp/test.pbf"
 	let header = runGet getHeader handle
-	let terms = map (uToString) $ toList $ getVal (fst header) Header.term 
+	let terms = getVal (fst header) Header.term 
 	let sizes = deltaDecode $ toList $ getVal (fst header) Header.size 
-	case (elemIndex s terms) of
+	case (elemIndexL (uFromString s) terms) of
 		Just i -> do
 			let offset = (snd header) + (foldl1 (+) (take i sizes))
 			let entrySize = sizes !! i
 			let entry = runGet (getEntry offset entrySize) handle
 			print entry
 		Nothing -> print "Does not exist"
+
 
 getHeader :: Get (Header.Header, Int64)
 getHeader = do
