3c2804f3b6981582790998c68638169413b48b98
Matthew Brown <matt@ederoyd.co.uk>
2014-05-15 22:52:58 +0100

    Improved the output of the console seach app

--- src/Main-Search.hs
+++ src/Main-Search.hs
@@ -5,6 +5,11 @@ import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import System.IO (hPutStrLn, stderr)
 import Control.Monad(when)
+import PB.Index.Entry
+import PB.Index.Tag
+import Common
+import Data.Foldable (toList)
+import Text.ProtocolBuffers.Basic (uToString)
 
 main :: IO ()
 main = do 
@@ -13,11 +18,29 @@ main = do
   let indexFile = head args
   let term = args !! 1
   results <- search indexFile term
-  mapM_ (print) results
+  mapM_ (printEntry) results
+
+printEntry entry = do
+  let t = toList $ tags entry
+  putStrLn $ ""
+  putStrLn $ "##################################################"
+  putStrLn $ "# Term:        " ++ (uToString $ term entry)
+  putStrLn $ "# Latitude:    " ++ (uToString $ latitude entry)
+  putStrLn $ "# Longitude:   " ++ (uToString $ longitude entry)
+  putStrLn $ "# Source:      " ++ (uToString $ src entry)
+  putStrLn $ "# Rank:        " ++ (uToString $ rank entry)
+  putStrLn $ "# Type:        " ++ (uToString $ type' entry)
+  mapM_ (printTag) t
+  putStrLn $ "##################################################"
+  putStrLn $ ""
+  where
+    printTag tag = do
+      let p = sParseTerm
+      putStrLn $ "# Tag:         " ++ (uToString $ key tag) ++ " = " ++ (uToString $ value tag) 
 
 showUsage :: IO ()
 showUsage = do
-      hPutStrLn stderr "usage: indexFile term"
-      hPutStrLn stderr "example: geo-search geodata.idx 'LEEDS'"
-      exitFailure
+  hPutStrLn stderr "usage: indexFile term"
+  hPutStrLn stderr "example: geo-search geodata.idx 'LEEDS'"
+  exitFailure
 
