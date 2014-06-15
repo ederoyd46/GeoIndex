355bec620397e2d8f614ca03ae3242f0c2dffe7c
Matthew Brown <matt@ederoyd.co.uk>
2014-06-15 16:48:46 +0100

    Added links to maps

--- src/Main-Search.hs
+++ src/Main-Search.hs
@@ -27,18 +27,21 @@ printEntry entry = do
   let t = toList $ tags entry
   putStrLn $ ""
   putStrLn $ "##################################################"
-  putStrLn $ "# Term:        " ++ (uToString $ term entry)
-  putStrLn $ "# Latitude:    " ++ (uToString $ latitude entry)
-  putStrLn $ "# Longitude:   " ++ (uToString $ longitude entry)
-  putStrLn $ "# Source:      " ++ (uToString $ src entry)
-  putStrLn $ "# Rank:        " ++ (uToString $ rank entry)
-  putStrLn $ "# Type:        " ++ (uToString $ type' entry)
+  putStrLn $ "# Term:                   " ++ (uToString $ term entry)
+  putStrLn $ "# Latitude:               " ++ (uToString $ latitude entry)
+  putStrLn $ "# Longitude:              " ++ (uToString $ longitude entry)
+  putStrLn $ "# Source:                 " ++ (uToString $ src entry)
+  putStrLn $ "# Rank:                   " ++ (uToString $ rank entry)
+  putStrLn $ "# Type:                   " ++ (uToString $ type' entry)
   mapM_ (printTag) t
+  putStrLn $ "# Open Streetmap:         " ++ ("http://www.openstreetmap.org/#map=15/" ++ (uToString $ latitude entry) ++ "/" ++ (uToString $ longitude entry))
+  putStrLn $ "# Here:                   " ++ ("http://here.com/" ++ (uToString $ latitude entry) ++ "," ++ (uToString $ longitude entry) ++ ",15,0,0,normal.day")
+  putStrLn $ "# Google Maps:            " ++ ("http://www.google.co.uk/maps/@" ++ (uToString $ latitude entry) ++ "," ++ (uToString $ longitude entry) ++ ",15z")
   putStrLn $ "##################################################"
   where
     printTag tag = do
       let p = sParseTerm
-      putStrLn $ "# Tag:         " ++ (uToString $ key tag) ++ " = " ++ (uToString $ value tag) 
+      putStrLn $ "# Tag:                    " ++ (uToString $ key tag) ++ " = " ++ (uToString $ value tag) 
 
 showUsage :: IO ()
 showUsage = do
